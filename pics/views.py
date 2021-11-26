49389074a870f22ef1342a5e4302c5d856115f42
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-11-27 01:55:18 +0300

    updated views.py

--- pics/views.py
+++ pics/views.py
@@ -1,7 +1,15 @@
 import datetime as dt
 from django.http  import HttpResponse
-
 from django.shortcuts import render
+from .models import Images
+
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+
+
+def index(request):
+    Image = Images.objects.all()
+    ctx = {'Image':Image}
+    return render(request, 'all-pics/index.html', ctx)
