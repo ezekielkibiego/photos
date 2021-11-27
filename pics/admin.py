038262621c0e7bd9078e07114433d2b5511d924e
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-11-27 23:19:23 +0300

    created category class

--- pics/admin.py
+++ pics/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Images
+from .models import Category, Images
 
+admin.site.register(Category)
 admin.site.register(Images)
--- pics/models.py
+++ pics/models.py
@@ -2,12 +2,31 @@ from django.db import models
 import datetime as dt
 from cloudinary.models import CloudinaryField
 
+class Category(models.Model):
+    name = models.CharField(max_length=40, null=True)
+
+    
+    def save_category(self):
+        self.save()
+
+    
+    def update_category(self, name):
+        self.name = name
+        self.save()
+
+    
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
 
 class Images(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     # location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     image = CloudinaryField('image')
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -20,7 +39,7 @@ class Images(models.Model):
         self.name = name
         self.description = description
         # self.location = location
-        # self.category = category
+        self.category = Category
         self.save()
 
     # get all images
@@ -33,3 +52,14 @@ class Images(models.Model):
     def search_by_name(cls,search_term):
         pics = cls.objects.filter(name__icontains=search_term)
         return pics
+
+    @classmethod
+    def filter_by_category(cls, category):
+        images = Images.objects.filter(category__name=category)
+        return images
+
+    def delete_image(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
