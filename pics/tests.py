73d58b246fbaeea2a09d8d6a187fe7b15f5abdb1
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-11-29 10:42:11 +0300

    added test cases

--- pics/tests.py
+++ pics/tests.py
@@ -1,9 +1,53 @@
 from django.test import TestCase
 from pics.models import Location, Category, Images
 
-# Create your tests here.
-class ImagesTestCase(TestCase):
 
+class LocationTestCase(TestCase):
+
+    def setUp(self):
+        """
+        Create a location for testing
+        """
+        Location.objects.create(name="Test Location")
+
+    def test_location_name(self):
+        """
+        Test that the location name is correct
+        """
+        location = Location.objects.get(name="Test Location")
+        self.assertEqual(location.name, "Test Location")
+
+    def test_location_str(self):
+        """
+        Test that the location string representation is correct
+        """
+        location = Location.objects.get(name="Test Location")
+        self.assertEqual(str(location), "Test Location")
+
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        """
+        Create a category for testing
+        """
+        Category.objects.create(name="Test Category")
+
+    def test_category_name(self):
+        """
+        Test that the category name is correct
+        """
+        category = Category.objects.get(name="Test Category")
+        self.assertEqual(category.name, "Test Category")
+
+    def test_category_str(self):
+        """
+        Test that the category string representation is correct
+        """
+        category = Category.objects.get(name="Test Category")
+        self.assertEqual(str(category), "Test Category")
+
+
+class ImagesTestCase(TestCase):
     def setUp(self):
         """
         Create a image for testing
@@ -11,51 +55,36 @@ class ImagesTestCase(TestCase):
         Images.objects.create(
             name="Test Image",
             description="Test Description",
-            location=Location.objects.create(name="Test Location"),
-            category=Category.objects.create(name="Test Category"),
-            image="http://test.com/test.jpg",
-            created_at=None
+            location=Location.objects.create(name="Image Location"),
+            category=Category.objects.create(name="Image Category"),
         )
-
-    def test_image_name(self):
+    def test_images_name(self):
         """
         Test that the image name is correct
         """
         image = Images.objects.get(name="Test Image")
-        self.assertEqual(Images.name, "Test Image")
-
-    def test_image_description(self):
+        self.assertEqual(image.name, "Test Image")
+    def test_images_description(self):
         """
         Test that the image description is correct
         """
         image = Images.objects.get(name="Test Image")
-        self.assertEqual(Images.description, "Test Description")
-
-    def test_image_location(self):
+        self.assertEqual(image.description, "Test Description")
+    def test_images_location(self):
         """
         Test that the image location is correct
         """
         image = Images.objects.get(name="Test Image")
-        self.assertEqual(Images.location.name, "Test Location")
-
-    def test_image_category(self):
+        self.assertEqual(image.location.name, "Image Location")
+    def test_images_category(self):
         """
         Test that the image category is correct
         """
         image = Images.objects.get(name="Test Image")
-        self.assertEqual(Images.category.name, "Test Category")
-
-    def test_image_image(self):
-        """
-        Test that the image image is correct
-        """
-        image = Images.objects.get(name="Test Image")
-        self.assertEqual(Images.image, "http://test.com/test.jpg")
-
-
-    def test_image_str(self):
+        self.assertEqual(image.category.name, "Image Category")
+    def test_images_str(self):
         """
         Test that the image string representation is correct
         """
         image = Images.objects.get(name="Test Image")
-        self
+        self.assertEqual(str(image), "Test Image")
